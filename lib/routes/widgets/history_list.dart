5f189d51c24fde861be1bc6a634a8bd189e4eb46
arunbabuvn <arunbabuvn@gmail.com>
2023-10-28 18:45:46 +0530

    files renamed

--- lib/routes/widgets/history_list.dart
+++ lib/routes/widgets/history_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:resumify/routes/resumeData.dart';
+import 'package:resumify/routes/resume_data.dart';
 
 class HistoryList extends StatelessWidget {
   const HistoryList({super.key});
